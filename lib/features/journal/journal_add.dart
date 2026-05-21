60a9885b4b3ffaef245fbe47021a3e45561a3c22
Faradis Yulianto <faradisy20@gmail.com>
2026-05-22 04:02:16 +0700

    feat: add profile page

---
+++ lib/features/journal/journal_add.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JournalAdd extends StatefulWidget {
+  const JournalAdd({super.key});
+
+  @override
+  State<JournalAdd> createState() => _JournalAddState();
+}
+
+class _JournalAddState extends State<JournalAdd> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
