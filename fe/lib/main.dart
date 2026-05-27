c33237e64575123e9dab8bb8848c5220759a974b
Faradis Yulianto <faradisy20@gmail.com>
2026-05-28 00:36:01 +0700

    feat: integrate unfinished Supabase authentication and user profile management

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+
+// Router
 import 'package:hackathon/router.dart';
 
-void main() {
+// Supabase & DotEnv
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  
+  try {
+    await dotenv.load(fileName: ".env");
+    print("DotEnv berhasil dimuat!");
+  } catch (e) {
+    print("Gagal memuat DotEnv, pastikan file .env ada di aset pubspec.yaml: $e");
+  }
+
+  final String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  final String supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'] ?? '';
+
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
+  
   runApp(const MyApp());
 }
 
@@ -18,4 +41,4 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-} 
+}
